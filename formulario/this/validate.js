c5799a4a12435bfb7ad961bad6377f2e4c3fd265
Ed2902 <118938899+Ed2902@users.noreply.github.com>
2023-04-02 22:14:18 -0600

    Add files via upload

--- formulario/this/validate.js
+++ formulario/this/validate.js
@@ -12,28 +12,29 @@ const validateTel = (cad) =>{
 
 class Validate{
     validNames(values){
-        const nombresRX = /^([a-zA-ZÀ-ÖØ-öø-ÿ]{3,25})([\s]?)([a-zA-ZÀ-ÖØ-öø-ÿ]{0,25}){1,3}$/g;
-        const respuesta = values.match(nombresRX) ? true : false;
+        const nombresFo = /^([a-zA-ZÀ-ÖØ-öø-ÿ]{3,25})([\s]?)([a-zA-ZÀ-ÖØ-öø-ÿ]{0,25}){1,3}$/g;
+        const respuesta = values.match(nombresFo) ? true : false;
         return respuesta;
     }
 
     validMail(values){
-        const correoRX = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-        const respuesta = values.match(correoRX)? true : false;
+        const correoFo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+        const respuesta = values.match(correoFo)? true : false;
         return respuesta;
     }
 
     validTel(values){
-        const TelRX = /^(((300|301|302|303|304|324|305|310|311|312|313|314|320|321|322|323|315|316|317|318|319|350|351)[0-9]{7}))$/g; 
-        const respuesta = values.match(TelRX) ? true : false;
+        const TelFo = /^(((300|301|302|303|304|324|305|310|311|312|313|314|320|321|322|323|315|316|317|318|319|350|351)[0-9]{7}))$/g; 
+        const respuesta = values.match(TelFo) ? true : false;
         //devuelve true o false
         return respuesta;
     }
 
     validText(values){
-        const textRX =/^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\/\*\-\+\.\)\(\&\%\$\#\@\!]).{6,32})$/;
-        const respuesta = values.match(textRX) ? true : false;
+        const textF =/^([a-zA-ZÀ-ÖØ-öø-ÿ]+\s){6,32}$/g;
+        const respuesta = values.match(textF) ? true : false;
         return respuesta;
+
     }
 
     validform = (objeto) => {
